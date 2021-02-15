4b0044a4c1140219ef12f640fc74121f947fcb29
BlueSword03 <bluesword2003@gmail.com>
2021-02-15 10:16:11 +0800

    Initial commit

---
+++ device.mk
@@ -0,0 +1,42 @@
+#
+# Copyright (C) 2020 The exTHmUI Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+DEVICE_PATH := device/nokia/DRG
+
+# Set Shipping API level
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o.mk)
+
+# Inherit from nokia sdm660-common
+$(call inherit-product, device/nokia/sdm660-common/sdm660.mk)
+
+# Setup dalvik vm configs
+$(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
+
+# Get non-open-source specific aspects
+$(call inherit-product, vendor/nokia/DRG/DRG-vendor.mk)
+
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += \
+    $(DEVICE_PATH)/overlay
+
+PRODUCT_PACKAGES += \
+    NoCutoutOverlay
+
+# Ramdisk
+PRODUCT_PACKAGES += \
+    init.DRG.target.rc
+
+# Get non-open-source specific aspects
+$(call inherit-product, vendor/nokia/DRG/DRG-vendor.mk)
